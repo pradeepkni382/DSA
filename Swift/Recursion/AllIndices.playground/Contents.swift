53e3219eb4d1f1aa7bc520ae8298d0812785d0f3
pradeepkni382 <pradeepkni382@gmail.com>
2023-07-15 23:59:30 +0530

    wsdas

--- Swift/Recursion/AllIndices.playground/Contents.swift
+++ Swift/Recursion/AllIndices.playground/Contents.swift
@@ -1,3 +1,19 @@
 import UIKit
 
 
+func collectAllIndices(inputArray: [Int], elementTobeFound:Int, currentIndex: Int, totalInstanecsFoundSoFar: Int) -> [Int] {
+    if (currentIndex == inputArray.count) {
+        return []
+    }
+    if inputArray[currentIndex] == elementTobeFound {
+        var resultingArray = collectAllIndices(inputArray: inputArray, elementTobeFound: elementTobeFound, currentIndex: currentIndex + 1, totalInstanecsFoundSoFar: totalInstanecsFoundSoFar + 1)
+        resultingArray[totalInstanecsFoundSoFar] = currentIndex
+        return resultingArray
+    } else {
+        var resultingArray = collectAllIndices(inputArray: inputArray, elementTobeFound: elementTobeFound, currentIndex: currentIndex + 1, totalInstanecsFoundSoFar: totalInstanecsFoundSoFar)
+        return resultingArray
+    }
+}
+
+let inputArrayc = [2,3,6,9,8,3,2,3,6,4]
+collectAllIndices(inputArray: inputArrayc, elementTobeFound: 3, currentIndex: 0, totalInstanecsFoundSoFar: 0)
