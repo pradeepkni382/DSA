28d564702b477ed2e67f48f10b5aa78093853ddd
pradeepkni382 <pradeepkni382@gmail.com>
2023-07-16 00:06:04 +0530

    w29/Swift/Recursion/Find all instances

--- Swift/Recursion/AllIndices.playground/Contents.swift
+++ Swift/Recursion/AllIndices.playground/Contents.swift
@@ -1,19 +1,23 @@
 import UIKit
 
+/*
+ https://youtu.be/bQkwHBaNioE
+ 
+ */
 
-func collectAllIndices(inputArray: [Int], elementTobeFound:Int, currentIndex: Int, totalInstanecsFoundSoFar: Int) -> [Int] {
-    if (currentIndex == inputArray.count) {
-        return []
+func collectAllIndices(inputArray: [Int], elementTobeFound: Int, currentIndex: Int, totalInstanecsFoundSoFar: inout [Int]) {
+    if currentIndex == inputArray.count {
+        return
     }
     if inputArray[currentIndex] == elementTobeFound {
-        var resultingArray = collectAllIndices(inputArray: inputArray, elementTobeFound: elementTobeFound, currentIndex: currentIndex + 1, totalInstanecsFoundSoFar: totalInstanecsFoundSoFar + 1)
-        resultingArray[totalInstanecsFoundSoFar] = currentIndex
-        return resultingArray
-    } else {
-        var resultingArray = collectAllIndices(inputArray: inputArray, elementTobeFound: elementTobeFound, currentIndex: currentIndex + 1, totalInstanecsFoundSoFar: totalInstanecsFoundSoFar)
-        return resultingArray
+        totalInstanecsFoundSoFar.append(currentIndex)
     }
+    collectAllIndices(inputArray: inputArray, elementTobeFound: elementTobeFound, currentIndex: currentIndex + 1, totalInstanecsFoundSoFar: &totalInstanecsFoundSoFar)
 }
 
-let inputArrayc = [2,3,6,9,8,3,2,3,6,4]
-collectAllIndices(inputArray: inputArrayc, elementTobeFound: 3, currentIndex: 0, totalInstanecsFoundSoFar: 0)
+var totalInstancesFound: [Int] = []
+let inputArray = [2, 3, 6, 9, 8, 3, 2, 3, 6, 4]
+collectAllIndices(inputArray: inputArray, elementTobeFound: 3, currentIndex: 0, totalInstanecsFoundSoFar: &totalInstancesFound)
+
+print(totalInstancesFound)  // Output: [1, 5, 7]
+
